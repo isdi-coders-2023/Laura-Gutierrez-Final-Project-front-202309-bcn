d83619dcb38633229434469cbe3efc1c5374b874
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-29 20:03:00 +0100

    Add navigation component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import AppStyled from "./AppStyled";
 import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
 
 const App = (): React.ReactElement => {
   return (
     <AppStyled>
       <Header />
+      <Navigation />
       <main>
         <h1>Herbolarium</h1>
         <Routes>
