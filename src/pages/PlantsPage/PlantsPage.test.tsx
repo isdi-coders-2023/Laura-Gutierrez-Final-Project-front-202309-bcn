607e98e4113ee745469fc9dde04e420f508cc378
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-02 21:14:58 +0100

    Test PlantsPage page

--- src/pages/PlantsPage/PlantsPage.test.tsx
+++ src/pages/PlantsPage/PlantsPage.test.tsx
@@ -4,9 +4,9 @@ import PlantsPage from "./PlantsPage";
 import customRender from "../../testUtils/CustomRender";
 
 describe("When recieves a list of Records", () => {
-  test("It should show `This is your online inventory of ethnobotanical plants. Mark if they're in your pantry and learn all about them.` in a heading", () => {
+  test("It should show `This is your online inventory of ethnobotanical plants. Mark whether they're in your pantry and learn all about them.` in a heading", () => {
     const expectedHeadingText =
-      "This is your online inventory of ethnobotanical plants. Mark if they're in your pantry and learn all about them.";
+      "This is your online inventory of ethnobotanical plants. Mark whether they're in your pantry and learn all about them.";
 
     customRender(<PlantsPage />);
 
