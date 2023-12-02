79c1c12851d28910a6c1e257e3ca1b2f63850bfa
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-02 20:52:05 +0100

    Add width and height to images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,12 @@ const Header = () => {
   return (
     <HeaderStyled>
       <img
+        className="header-image"
         src="images/header3.jpg"
         alt="Herbolarium logo with a cauldrum and herbs"
         title="Herbolarium"
+        width="500"
+        height="170"
       />
     </HeaderStyled>
   );
