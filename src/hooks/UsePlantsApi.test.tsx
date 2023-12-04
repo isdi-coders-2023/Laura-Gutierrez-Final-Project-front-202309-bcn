b92f550a457e1a49de92fa5f32996b0870b6cd5f
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-04 12:41:36 +0100

    Modify UseEffect method in PlantsPage

--- src/hooks/UsePlantsApi.test.tsx
+++ src/hooks/UsePlantsApi.test.tsx
@@ -2,11 +2,12 @@ import { renderHook } from "@testing-library/react";
 import { plantsMock } from "../mocks/plantsMock";
 import usePlantsApi from "./UsePlantsApi";
 import { providerWrapper } from "../testUtils/CustomRender";
+import { PlantsStateStructure } from "../store/features/types";
 
 describe("Given a usePlantsApi custom hook", () => {
   describe("When it gets the information of Oregano and Salvia", () => {
     test("Then it should show Oregano and Salvia's information", async () => {
-      const expectedPlants = plantsMock;
+      const expectedPlants: PlantsStateStructure = { plants: plantsMock };
 
       const {
         result: {
