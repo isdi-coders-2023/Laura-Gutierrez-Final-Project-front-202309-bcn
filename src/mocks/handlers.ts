b4a478b113fb4e2e39580ed26958c0e2df9140bc
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-05 17:58:29 +0100

    Add delete handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,6 +7,12 @@ const handler = [
   http.get(`${urlApi}/plants`, async () => {
     return HttpResponse.json({ plants: plantsMock });
   }),
+  http.delete(
+    `${import.meta.env.VITE_API_URL}/plants/6566158cd11a3f8f1075c7a1`,
+    () => {
+      return HttpResponse.json({});
+    },
+  ),
 ];
 
 export default handler;
