8297fd4efbae3099a93b342f49db2bd5739d9353
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-07 12:15:21 +0100

    Add AddPlantPage component

---
+++ src/pages/AddPlantPage/AddPlantPage.tsx
@@ -0,0 +1,9 @@
+const AddPlantPage = (): React.ReactElement => {
+  return (
+    <main>
+      <h2>Can’t find your plant in our inventory? Add it here:</h2>
+    </main>
+  );
+};
+
+export default AddPlantPage;
