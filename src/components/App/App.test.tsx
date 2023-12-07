32a49dad5fabc5cc55161b2857534e8994f96119
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-07 16:39:08 +0100

    Test Form component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,19 +1,40 @@
 import { screen } from "@testing-library/react";
-import customRender from "../../testUtils/CustomRender";
+import {
+  customRender,
+  customRenderWithoutBrowserRouter,
+} from "../../testUtils/CustomRender";
 import App from "./App";
-import { plantsMock } from "../../store/features/plants/mocks/plantsMock";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given an App component", () => {
   describe("When it's rendered", () => {
     test("Then it should show 'Herbolarium logo with a cauldrum and herbs' logo", () => {
       const expectedAltText = "Herbolarium logo with a cauldrum and herbs";
-      const mockData = plantsMock;
 
-      customRender(<App />, mockData);
+      customRender(<App />);
 
       const headerLogo = screen.getByAltText(expectedAltText);
 
       expect(headerLogo).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with a PlantsPage /plants path", () => {
+    test("Then it should show the text 'This is your online inventory of ethnobotanical plants. Mark whether they're in your pantry and learn all about them.' in a heading", () => {
+      const expectedHeadingText =
+        "This is your online inventory of ethnobotanical plants. Mark whether they're in your pantry and learn all about them.";
+
+      customRenderWithoutBrowserRouter(
+        <MemoryRouter initialEntries={["/plants"]}>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const heading = screen.getByRole("heading", {
+        name: expectedHeadingText,
+      });
+
+      expect(heading).toBeInTheDocument();
     });
   });
 });
