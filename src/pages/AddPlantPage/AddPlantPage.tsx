8d999be74008a2b204a05319b0cfc913158516e2
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-08 11:14:48 +0100

    Add submitAction into PlantForm in AddPlantPage page

--- src/pages/AddPlantPage/AddPlantPage.tsx
+++ src/pages/AddPlantPage/AddPlantPage.tsx
@@ -1,14 +1,22 @@
 import PlantForm from "../../components/PlantForm/PlantForm";
+import usePlantsApi from "../../hooks/UsePlantsApi";
+import { PlantsStructure } from "../../store/features/types";
 import AddPlantPageStyled from "./AddPlantPageStyled";
 
 const AddPlantPage = (): React.ReactElement => {
+  const { addNewPlant } = usePlantsApi();
+
+  const addCurrentPlant = (newPlant: PlantsStructure) => {
+    addNewPlant(newPlant);
+  };
+
   return (
     <AddPlantPageStyled>
       <div className="page">
         <h2 className="page-text">
           Can’t find your plant in our inventory? Add it here:
         </h2>
-        <PlantForm />
+        <PlantForm submitAction={addCurrentPlant} />
       </div>
     </AddPlantPageStyled>
   );
