4f842e928926e7e88b6893f894a1a1614ddd919c
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-09 22:24:17 +0100

    Test PlantFrom's inputs

--- src/components/PlantForm/PlantForm.test.tsx
+++ src/components/PlantForm/PlantForm.test.tsx
@@ -4,13 +4,13 @@ import PlantForm from "./PlantForm";
 import userEvent from "@testing-library/user-event";
 
 describe("Given a PlantForm component", () => {
-  const actionOnClick = vi.fn();
+  const submitAction = vi.fn();
 
   describe("When it's rendered", () => {
     test("Then it should show a 'Name:' label text.", () => {
       const expectedLabelText = "Name:";
 
-      customRender(<PlantForm submitAction={actionOnClick} />);
+      customRender(<PlantForm submitAction={submitAction} />);
 
       const labelText = screen.getByLabelText(expectedLabelText);
 
@@ -21,7 +21,7 @@ describe("Given a PlantForm component", () => {
   test("Then it should show a button with the text 'Upload plant'.", () => {
     const expectedButtonText = "Upload plant";
 
-    customRender(<PlantForm submitAction={actionOnClick} />);
+    customRender(<PlantForm submitAction={submitAction} />);
 
     const buttonText = screen.getByText(expectedButtonText);
 
@@ -32,7 +32,7 @@ describe("Given a PlantForm component", () => {
     test("Then it should show the written data in its corresponding input.", async () => {
       const expectedInputText = "Oregano";
 
-      customRender(<PlantForm submitAction={actionOnClick} />);
+      customRender(<PlantForm submitAction={submitAction} />);
 
       const labelText = screen.getByLabelText("Name:");
 
@@ -45,13 +45,76 @@ describe("Given a PlantForm component", () => {
   });
 
   describe("When user clicks/taps on the button 'Upload plant'", () => {
-    test("Then it should call the form's onSubmit action", () => {
-      customRender(<PlantForm submitAction={actionOnClick} />);
+    test("Then it should call the form's submitAction action", () => {
+      customRender(<PlantForm submitAction={submitAction} />);
 
       const plantForm = screen.getByLabelText("Name:");
       fireEvent.submit(plantForm);
 
-      expect(actionOnClick).toHaveBeenCalled();
+      expect(submitAction).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it is rendered and the user clicks on the button 'Upload plant'", () => {
+    test("Then it should call its submitAction function", async () => {
+      customRender(<PlantForm submitAction={submitAction} />);
+
+      const plantForm = screen.getByLabelText("Name:");
+      const addPlantButton = screen.getByRole("button", {
+        name: "Upload plant",
+      });
+
+      await userEvent.click(addPlantButton);
+
+      fireEvent.submit(plantForm);
+
+      expect(submitAction).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it is rendered and the user fills every input and clicks the 'Upload plant' button", () => {
+    test("Then it should call its submitAction function", async () => {
+      const expectedInputText = "Oregano";
+      const expectedButtonText = "Upload plant";
+
+      customRender(<PlantForm submitAction={submitAction} />);
+
+      const inputPlantElement = screen.getByLabelText("Name:");
+      await userEvent.type(inputPlantElement, expectedInputText);
+
+      const inputScientificNameElement =
+        screen.getByLabelText("Scientific name:");
+      await userEvent.type(inputScientificNameElement, expectedInputText);
+
+      const inputUseElement = screen.getByLabelText(
+        "Use (culinary/medicinal/poison):",
+      );
+      await userEvent.type(inputUseElement, expectedInputText);
+
+      const inputHabitatElement = screen.getByLabelText("Habitat:");
+      await userEvent.type(inputHabitatElement, expectedInputText);
+
+      const inputImageElement = screen.getByLabelText("Image URL:");
+      await userEvent.type(inputImageElement, expectedInputText);
+
+      const inputPropertiesElement = screen.getByLabelText("Properties:");
+      await userEvent.type(inputPropertiesElement, expectedInputText);
+      0;
+      const inputHowToElement = screen.getByLabelText("How to use it:");
+      await userEvent.type(inputHowToElement, expectedInputText);
+
+      const inputIsPoisonousElement = screen.getByLabelText(
+        "If poisonous, how? (optional):",
+      );
+      await userEvent.type(inputIsPoisonousElement, expectedInputText);
+
+      const formButtonElement = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      await userEvent.click(formButtonElement);
+
+      expect(submitAction).toHaveBeenCalled();
     });
   });
 });
