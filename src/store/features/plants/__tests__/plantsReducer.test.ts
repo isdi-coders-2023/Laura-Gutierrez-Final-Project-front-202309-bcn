f239d9dbb1f9e10635a15b63dc0272e76f5fbc85
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-11 12:55:59 +0100

    Test loadSelectedPlant mini reducer

--- src/store/features/plants/__tests__/plantsReducer.test.ts
+++ src/store/features/plants/__tests__/plantsReducer.test.ts
@@ -1,6 +1,11 @@
 import { plantsMock } from "../mocks/plantsMock";
-import { plantsReducer, loadPlantsActionCreator } from "../plantsSlice";
+import {
+  plantsReducer,
+  loadPlantsActionCreator,
+  loadSelectedPlantActionCreator,
+} from "../plantsSlice";
 import { PlantsStateStructure } from "../../types";
+import { plantMock } from "../mocks/plantMock";
 
 describe("Given a plantsReducer", () => {
   describe("When it receives an empty list of plants", () => {
@@ -27,6 +32,39 @@ describe("Given a plantsReducer", () => {
       );
 
       expect(newListState.plants).toStrictEqual(plantsList);
+    });
+  });
+
+  describe("When it receives and empty state of selected and load selected plant action with the plant 'Oregano'", () => {
+    test("Then it should return the new state with the plant 'Oregano'", () => {
+      const initialState: PlantsStateStructure = {
+        plants: plantsMock,
+        selectedPlant: {
+          _id: "",
+          name: "",
+          scientificName: "",
+          use: "",
+          properties: "",
+          howToUse: "",
+          imageUrl: "",
+          isPoisonous: "",
+          habitat: "",
+        },
+      };
+
+      const expectedNewState: PlantsStateStructure = {
+        plants: plantsMock,
+        selectedPlant: plantMock,
+      };
+
+      const loadSelectedAction = loadSelectedPlantActionCreator(plantMock);
+
+      const newState: PlantsStateStructure = plantsReducer(
+        initialState,
+        loadSelectedAction,
+      );
+
+      expect(newState).toStrictEqual(expectedNewState);
     });
   });
 });
