78326047015802b7d557098c2ba3294f0d0f877a
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-11 11:55:05 +0100

    Delete false positive test

--- src/components/PlantCard/PlantCard.test.tsx
+++ src/components/PlantCard/PlantCard.test.tsx
@@ -3,8 +3,6 @@ import { plantsMock } from "../../store/features/plants/mocks/plantsMock";
 import { customRender } from "../../testUtils/CustomRender";
 import PlantCard from "./PlantCard";
 import userEvent from "@testing-library/user-event";
-import server from "../../mocks/node";
-import { errorHandlers } from "../../mocks/handlers";
 
 describe("Given a PlantCard component", () => {
   describe("When it receives a 'Oregano' card", () => {
@@ -51,26 +49,6 @@ describe("Given a PlantCard component", () => {
       const expectedMessage = "Plant removed from our inventory!";
 
       expect(screen.getByText(expectedMessage)).toBeInTheDocument();
-    });
-
-    test("Then it should show the negative feedback message 'Error: Couldn’t remove plant. Please try again.'", async () => {
-      server.use(...errorHandlers);
-
-      const deleteButtonText = "Delete";
-      const expectedErrorMessage =
-        "Error: Couldn’t remove plant. Please try again.";
-
-      customRender(<PlantCard plant={plantsMock[0]} />);
-
-      const deleteButton = screen.getByRole("button", {
-        name: deleteButtonText,
-      });
-
-      await userEvent.click(deleteButton);
-
-      const errorText = screen.getByText(expectedErrorMessage);
-
-      expect(errorText).toBeInTheDocument();
     });
   });
 });
